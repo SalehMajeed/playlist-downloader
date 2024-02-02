cc7af46348b2ee6448a6101e1dfcb03d68083994
sameer khan <sameerkhanaa5@gmail.com>
2024-02-03 01:25:57 +0530

    added react routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,19 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
 import "./App.css";
 import PlaylistTable from "./Components/PlaylistTable";
 import UserInput from "./Components/UserInput";
 
 function App() {
-  const [playlistAddress, setPlaylistAddress] = useState("");
-  const [playlistData, setPlaylistData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      if (playlistAddress !== "") {
-        const playlist_url = encodeURIComponent(playlistAddress);
-        const fetchedReq = await axios.get(
-          `${process.env.REACT_APP_BASE_URL}/getInfo?playlistAddress=${playlist_url}`
-        );
-        const reqData = fetchedReq.data;
-        setPlaylistData(reqData.data);
-      }
-    };
-
-    fetchData();
-  }, [playlistAddress]);
-
   return (
-    <div className="App">
-      <UserInput setPlaylistAddress={setPlaylistAddress} />
-      <PlaylistTable playlistData={playlistData} />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<UserInput />} />
+          <Route path="/download-list" element={<PlaylistTable />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- frontend/src/Components/UserInput/UserInputCmpt.js
+++ frontend/src/Components/UserInput/UserInputCmpt.js
@@ -1,26 +1,50 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router";
 
-function UserInputCmpt({setPlaylistAddress}) {
+function UserInputCmpt() {
   const [userInput, setUserInput] = useState("");
+  const [loadingData, setLoadingData] = useState(false);
+  const [playlistAddress, setPlaylistAddress] = useState("");
+  const navigate = useNavigate();
 
-
-  const HandleInput = (event) =>{
-    const data = event.target.value
+  const HandleInput = (event) => {
+    const data = event.target.value;
     setUserInput(data);
   };
 
-  const HandleDownload = () =>{
+  const HandleDownload = async () => {
+    if (playlistAddress.trim() !== "") {
+      setLoadingData(true);
+      const playlist_url = encodeURIComponent(playlistAddress.trim());
+      const fetchedReq = await axios.get(
+        `${process.env.REACT_APP_BASE_URL}/getInfo?playlistAddress=${playlist_url}`
+      );
+      const reqData = fetchedReq.data;
+      setLoadingData(false);
+      navigate("/download-list", { state: { data: reqData.data } });
+    }
+
     setPlaylistAddress(userInput);
     setUserInput("");
   };
 
   return (
     <div>
-    <input type="text" placeholder="enter your text" value={userInput} onChange={HandleInput} />
-    <br/>
-    <button onClick={HandleDownload}>Get Your Playlist</button>
+      <input
+        type="text"
+        placeholder="enter your text"
+        value={userInput}
+        onChange={HandleInput}
+      />
+      <br />
+      <button onClick={HandleDownload} disabled={loadingData}>
+        Get Your Playlist
+      </button>
+
+      {loadingData && <h1>Loading....</h1>}
     </div>
   );
-};
+}
 
-export default UserInputCmpt
+export default UserInputCmpt;
